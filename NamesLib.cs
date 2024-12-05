37ad9949b1e5297dc766bb87e29929913fb9ad8c
Ryan JG <ryangregoire286@gmail.com>
2024-12-05 17:00:53 -0500

    Add project files.

---
+++ NamesLib.cs
@@ -0,0 +1,45 @@
+﻿namespace NameListLib
+{
+    public class NamesLib
+    {
+        public static List<string> Names = new List<string>();
+        public static Random r = new Random();
+
+
+        public static void SetName(List<string> name)
+        {
+            Names = name;
+
+        }
+
+
+
+        public static string RandName(string name1, 
+            string name2, string name3)
+        {
+
+
+            Names.Add(name1);
+            Names.Add(name2);
+            Names.Add(name3);
+
+            return Names[r.Next(Names.Count)];
+
+        }
+
+
+        public static string RandName2(string name1,
+            string name2, string name3)
+        {
+
+
+            Names.Add(name1);
+            Names.Add(name2);
+            Names.Add(name3);
+
+            return Names[r.Next(Names.Count)];
+
+        }
+
+    }
+}
